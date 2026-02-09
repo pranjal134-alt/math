b87ba85509b31c5301362a45063b91e003408634
pranjal134-alt <sharmaharishankar700@gmail.com>
2026-02-09 13:52:41 +0530

    new

---
+++ add.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int add(int a, int b)
+{
+    return a + b;
+}
+int main()
+{
+    cout << add(30, 87);
+    return 0;
+}
--- pascal.cpp
+++ pascal.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main()
 {
     int i, j, n;
+    cout << "enter the number = ";
     cin >> n;
     for (i = 0; i < n; i++)
     {
