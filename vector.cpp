b014fc2a7ef59e170d5853c39ab3dbb1b54d09bd
pranjal134-alt <sharmaharishankar700@gmail.com>
2026-02-10 16:08:47 +0530

    py

---
+++ vector.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> a;
+    a.push_back(1);
+    a.push_back(2);
+    a.push_back(3);
+    a.push_back(4);
+    // cout << a.size();
+    cout << a.size();
+    // cout << a.capacity();
+    cout << a.capacity();
+    a.pop_back();
+    // a.insert(int a.begin()+a.size()/2,6)
+    // a.insert(a.begin() + 2, 98);
+    // a.insert(a.end() - 2, 3) for (int i = 0; i < a.size(); i++)
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i] << " ";
+    }
+    // cout << a.front() << endl;
+    // cout << a.back() << endl;
+
+    return 0;
+}
