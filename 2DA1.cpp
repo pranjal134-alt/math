308f553b8fe1963a9f9cb6e37cf164374e3e5f3f
pranjal134-alt <sharmaharishankar700@gmail.com>
2026-02-25 14:28:15 +0530

    cpp

---
+++ 2DA1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //  int array[][4]={
+    //      {1,2,3,4},
+    //      {5,6,7,8},
+    //      {9,10,11,12}
+    //  };
+    int a[2][3] = {1, 2, 3, 4, 5, 6};
+    int m, n;
+    cin >> m >> n;
+    int arr[m][n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
